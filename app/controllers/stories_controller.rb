b2c96d546c1c1bdb64756b4d3f9f7c3b6fd29c21
Varange <glo.varange@protonmail.com>
2020-05-02 16:30:38 -0400

    csp and twitter icon

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -154,7 +154,7 @@ class StoriesController < ApplicationController
           "twitter:description" => @story.comments_count.to_s + " " +
                                    'comment'.pluralize(@story.comments_count),
           "twitter:image" => Rails.application.root_url +
-                             "apple-touch-icon-144.png",
+                             "apple-touch-icon.png",
         }
 
         if @story.user.twitter_username.present?
