adf2599d5d6b811eef95aaa8d9eae6c8aed06366
Varange <glo.varange@protonmail.com>
2020-05-13 17:21:59 -0400

    require auth for viewing stories directly

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,10 +1,13 @@
 class StoriesController < ApplicationController
   caches_page :show, if: CACHE_PAGE
 
-  before_action :require_logged_in_user_or_400,
-                :only => [:upvote, :downvote, :unvote, :hide, :unhide, :preview, :save, :unsave]
-  before_action :require_logged_in_user,
-                :only => [:destroy, :create, :edit, :fetch_url_attributes, :new, :suggest]
+  # must be logged in to view posts from direct urls
+  before_action :require_logged_in_user
+
+  #before_action :require_logged_in_user_or_400,
+  #              :only => [:upvote, :downvote, :unvote, :hide, :unhide, :preview, :save, :unsave]
+  #before_action :require_logged_in_user,
+  #              :only => [:destroy, :create, :edit, :fetch_url_attributes, :new, :suggest]
   before_action :verify_user_can_submit_stories, :only => [:new, :create]
   before_action :find_user_story, :only => [:destroy, :edit, :undelete, :update]
   before_action :find_story!, :only => [:suggest, :submit_suggestions]
